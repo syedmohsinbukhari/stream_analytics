d67a32329df15efeb64e1362ba48c0f983266ecc
Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>
2018-03-17 23:14:14 +0500

    DataAnalyzer class now has functions for generating histogram into a file

--- streamanalytics/analyzer.py
+++ streamanalytics/analyzer.py
@@ -1,14 +1,20 @@
 import json
 import os
+import datetime
+import math
 
 class DataAnalyzer:
     def __init__(self):
         pass
 
+    def add_fname_suffix(self, fname, suffix):
+        o_fname, o_fname_ext = os.path.splitext( os.path.basename(fname) )
+        o_fname = o_fname + suffix + o_fname_ext
+        return o_fname
+
     def convert_to_json(self, fname):
         f_dir_name = os.path.dirname(os.path.abspath(fname))
-        o_fname, o_fname_ext = os.path.splitext( os.path.basename(fname) )
-        o_fname = o_fname + '_json' + o_fname_ext
+        o_fname = self.add_fname_suffix(fname, '_json')
 
         with open(fname) as inp_f, \
                 open(os.path.join(f_dir_name, o_fname), 'w') as out_f:
@@ -18,23 +24,87 @@ class DataAnalyzer:
                 out_f.write(json_str)
                 out_f.write(os.linesep)
 
+        return os.path.join(f_dir_name, o_fname)
 
-    def construct_histogram(self, fname, time_field_name, bin_width):
+    def str_to_unix_time(self, data_time_str):
+        date_time_obj = datetime.datetime.strptime(data_time_str,
+                                                    '%m/%d/%Y %H:%M:%S')
+        date_time_unix = date_time_obj.timestamp()
+        return date_time_unix
+
+    def unix_time_to_str(self, unix_time):
+        return datetime.datetime.fromtimestamp(unix_time).strftime(
+                                                            '%m/%d/%Y %H:%M:%S')
+
+    def min_unix_time(self, f_name, time_field_name):
+        min_time = float('Inf')
+        with open(f_name) as f:
+            for line in f:
+                data_row = json.loads(line)
+                date_time = data_row[time_field_name]
+                date_time_unix = self.str_to_unix_time(date_time)
+
+                if date_time_unix<min_time:
+                    min_time = date_time_unix
+        return min_time
+
+    def get_bin_ind(self, unix_time, start_time, bin_width):
+        bin_index = math.floor( (unix_time-start_time)/bin_width )
+        return bin_index
+
+    def get_bin_start(self, bin_ind, start_time, bin_width):
+        return start_time + (bin_ind*bin_width)
+
+    def construct_histogram(self, f_name, time_field_name, min_time, bin_width):
+        time_hist = {}
+        with open(f_name) as f:
+            for line in f:
+                data_row = json.loads(line)
+                date_time = data_row[time_field_name]
+                date_time_unix = self.str_to_unix_time(date_time)
+                time_bin = self.get_bin_ind(date_time_unix, min_time, bin_width)
+
+                if time_bin in time_hist.keys():
+                    time_hist[time_bin]['count'] += 1
+                else:
+                    bin_label=self.get_bin_start(time_bin, min_time, bin_width)
+                    bin_label=self.unix_time_to_str(bin_label)
+                    time_hist[time_bin] = {
+                        'bin_start': bin_label,
+                        'count': 1
+                        }
+
+        return time_hist
+
+    def get_histogram(self, fname, time_field_name, bin_width,
+                        convert_to_json=False):
         f_dir_name = os.path.dirname(os.path.abspath(fname))
+        f_name = fname
+        if convert_to_json:
+            f_name = self.convert_to_json(f_name)
 
         bin_width = int(bin_width)
+        min_time = self.min_unix_time(f_name, time_field_name)
+        time_hist = self.construct_histogram(f_name, time_field_name,
+                                                min_time, bin_width)
 
-        min_time = float('Inf')
-        with open(fname) as f:
-            for line in f:
-                data_row = eval(line)
-                print(data_row)
-                break
+        o_fname = self.add_fname_suffix(f_name, '_hist')
+        if not os.path.exists(os.path.join(f_dir_name, o_fname)):
+            out_f = open(os.path.join(f_dir_name, o_fname), 'a')
+            out_f.close()
+
+        with open(os.path.join(f_dir_name, o_fname), 'w') as out_f:
+            time_hist_json = json.dumps(time_hist)
+            out_f.write(time_hist_json)
+        return time_hist_json
+
+    def plot_hist(self):
+        pass
+        
 
 def main():
     analyzer = DataAnalyzer()
-    # analyzer.construct_histogram('data/data.txt', 'date_time', bin_width=10)
-    analyzer.convert_to_json('data/data.txt')
+    analyzer.get_histogram('data/data.txt', 'date_time', bin_width=60)
 
 if __name__ == '__main__':
     main()
