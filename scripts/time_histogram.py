a8bdacb66db07c3a22f67dbc9028f99674af315e
Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>
2018-04-11 18:58:18 +0500

    added a clarity comment in time_histogram.py script

--- scripts/time_histogram.py
+++ scripts/time_histogram.py
@@ -5,10 +5,12 @@ import streamanalytics as ts
 
 def main():
     histo = ts.Histogrammer()
+    
+    # From raw data file
     time_hist = histo.get_histo('data/data.txt', 'date_time', bin_width=10)
     histo.plot_histo(time_hist)
 
-    # OR directly from file
+    # OR directly from a previously generated histogram file
     # histo.plot_histo_from_file('data/data_hist.txt')
 
 if __name__ == '__main__':
